89f4cadc52de3de03020bfd8bfac219d3233c57f
Frédérick FLORKOWSKI <fred.florkowski@outlook.fr>
2024-12-04 16:24:29 +0100

    try another method to resize

--- src/index.js
+++ src/index.js
@@ -2,14 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import MapRetailers from "./components/MapRetailers";
 import ListRetailers from "./components/ListRetailers";
-import { useEffect } from "react";
-import iframeResizer from "iframe-resizer";
 
 const App = () => {
-  useEffect(() => {
-    iframeResizer({}, "#react-iframe");
-  }, []);
-
   const retailers = [
     {
       slug: "dubai",
